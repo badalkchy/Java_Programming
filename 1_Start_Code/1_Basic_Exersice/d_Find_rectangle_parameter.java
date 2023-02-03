a6ac8ffc90ed5e5c4545b817544e3b6e37e4e1c3
Badal Chaudhary <87421888+badalkchy@users.noreply.github.com>
2023-02-03 13:23:51 +0530

    Add files via upload

---
+++ 1_Start_Code/1_Basic_Exersice/d_Find_rectangle_parameter.java
@@ -0,0 +1,18 @@
+package A_Basic_Exersice;
+
+import java.util.Scanner;
+import java.lang.*;
+import java.io.*;
+
+public class d_Find_rectangle_parameter {
+	public static void main(String[] args) throws java.lang.Exception{
+		int n,m;
+		System.out.print("Enter length :");
+		Scanner sc=new Scanner(System.in);
+		n=sc.nextInt();
+		System.out.print("Enter width :");
+		m=sc.nextInt();
+		System.out.print((n*2)+(m*2)+" Units");
+		
+	}
+}
