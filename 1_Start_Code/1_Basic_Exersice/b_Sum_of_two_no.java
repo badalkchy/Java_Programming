1d754ecfe02108b5bad783fd5c5eb72249d28531
Badal Chaudhary <87421888+badalkchy@users.noreply.github.com>
2023-02-03 13:18:58 +0530

    Add files via upload

---
+++ 1_Start_Code/1_Basic_Exersice/b_Sum_of_two_no.java
@@ -0,0 +1,16 @@
+package A_Basic_Exersice;
+import java.util.Scanner;
+import java.lang.*;
+import java.io.*;
+
+class b_Sum_of_two_no{
+	public static void main(String[] args) throws java.lang.Exception{
+		int a,b;
+		System.out.print("Enter the number :");
+		Scanner sc=new Scanner(System.in);
+		a=sc.nextInt();
+		System.out.print("Enter the number :");
+		b=sc.nextInt();
+		System.out.print(a+b);
+	}
+}
